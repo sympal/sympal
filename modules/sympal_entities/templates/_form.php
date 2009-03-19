916ebe14d2ea60e7c1fd90156aa91a24ba54437f
jwage <jonwage@gmail.com>
2009-03-19 04:38:34 +0000

    [1.0] sfSympalPlugin: Fixing entity custom path routing.
    git-svn-id: http://svn.symfony-project.org/plugins/sfSympalPlugin/trunk@16433 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/sympal_entities/templates/_form.php
+++ modules/sympal_entities/templates/_form.php
@@ -10,7 +10,7 @@
     <?php endif; ?>
 
     <div id="right">
-      <?php echo get_partial('sympal_entities/form_part', array('name' => 'Mapping', 'form' => $form, 'fields' => array('site_id', 'master_menu_item_id'))) ?>
+      <?php echo get_partial('sympal_entities/form_part', array('name' => 'Mapping', 'form' => $form, 'fields' => array('site_id', 'master_menu_item_id', 'custom_path'))) ?>
       <?php echo get_partial('sympal_entities/form_part', array('name' => 'Security', 'form' => $form, 'fields' => array('groups_list', 'permissions_list'))) ?>
       <?php echo get_partial('sympal_entities/form_part', array('name' => 'Rendering', 'form' => $form, 'fields' => array('layout', 'entity_template_id'))) ?>
     </div>
