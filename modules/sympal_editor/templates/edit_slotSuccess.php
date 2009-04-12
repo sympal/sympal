3189f76c3bd38ee14daa13e32f95184c87d85035
jwage <jonwage@gmail.com>
2009-04-12 05:57:58 +0000

    [1.0] sfSympalPlugin: Fixing css for flash notices/errors
    git-svn-id: http://svn.symfony-project.org/plugins/sfSympalPlugin/trunk@17236 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/sympal_editor/templates/edit_slotSuccess.php
+++ modules/sympal_editor/templates/edit_slotSuccess.php
@@ -1,16 +1,18 @@
-<?php echo get_sympal_flash() ?>
+<div id="edit_slot">
+  <?php echo get_sympal_flash() ?>
 
-<?php echo $form->renderFormTag(url_for('@sympal_save_content_slot?id='.$sf_request->getParameter('id')), array('id' => 'edit_content_slot_form_'.$sf_request->getParameter('id'), 'method' => 'post')) ?>
-  <input type="hidden" name="is_column" value="<?php echo $sf_request->getParameter('is_column') ?>" />
-  <input type="hidden" name="name" value="<?php echo $sf_request->getParameter('name') ?>" />
+  <?php echo $form->renderFormTag(url_for('@sympal_save_content_slot?id='.$sf_request->getParameter('id')), array('id' => 'edit_content_slot_form_'.$sf_request->getParameter('id'), 'method' => 'post')) ?>
+    <input type="hidden" name="is_column" value="<?php echo $sf_request->getParameter('is_column') ?>" />
+    <input type="hidden" name="name" value="<?php echo $sf_request->getParameter('name') ?>" />
 
-  <table>
-    <?php echo $form ?>
-  </table>
+    <table>
+      <?php echo $form ?>
+    </table>
 
-  <div id="save">
-    <input type="button" id="save_button" name="save" value="<?php echo __('Save') ?>" onClick="javascript: save_sympal_content_slot('<?php echo $sf_request->getParameter('id') ?>');" />
-  </div>
-  <div id="loading"></div>
-  <br style="clear: both;" />
-</form>
+    <div id="save">
+      <input type="button" id="save_button" name="save" value="<?php echo __('Save') ?>" onClick="javascript: save_sympal_content_slot('<?php echo $sf_request->getParameter('id') ?>');" />
+    </div>
+    <div id="loading"></div>
+    <br style="clear: both;" />
+  </form>
+</div>
