3075a5a2655dd894f056e0673d3e69bba360019f
jwage <jonwage@gmail.com>
2009-05-07 09:46:23 +0000

    [1.0] sfSympalPlugin: Implementing user voice for feedback.
    git-svn-id: http://svn.symfony-project.org/plugins/sfSympalPlugin/trunk@18036 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- templates/sympal.php
+++ templates/sympal.php
@@ -92,6 +92,23 @@
    </div>
    <!-- end footer -->
 
+   <script type="text/javascript">
+     var uservoiceJsHost = ("https:" == document.location.protocol) ? "https://uservoice.com" : "http://cdn.uservoice.com";
+     document.write(unescape("%3Cscript src='" + uservoiceJsHost + "/javascripts/widgets/tab.js' type='text/javascript'%3E%3C/script%3E"))
+   </script>
+   <script type="text/javascript">
+   UserVoice.Tab.show({ 
+     key: 'sympal',
+     host: 'sympal.uservoice.com', 
+     forum: 'general', 
+     alignment: 'left',
+     background_color:'#f00', 
+     text_color: 'white',
+     hover_color: '#06C',
+     lang: '<?php echo $sf_user->getCulture() ?>'
+   })
+   </script>
+
    <?php echo get_sympal_editor() ?>
  </body>
 </html>
