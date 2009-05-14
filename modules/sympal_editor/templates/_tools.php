2bd85101a3578fea019a65af417ed8f90697600b
jwage <jonwage@gmail.com>
2009-05-14 02:22:48 +0000

    corrected an issue with setting css display property to an invalid value
    git-svn-id: http://svn.symfony-project.org/plugins/sfSympalPlugin/trunk@18243 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/sympal_editor/templates/_tools.php
+++ modules/sympal_editor/templates/_tools.php
@@ -101,7 +101,7 @@ function preview_sympal_content_slot(id)
 function save_sympal_content_slot(id)
 {
   highlight_sympal_content_slot(id);
-  document.getElementById('loading').style.display = 'visible';
+  document.getElementById('loading').style.display = '';
   document.getElementById('loading').innerHTML = 'Loading...';
 
 	var url = "<?php echo url_for('@sympal_save_content_slot?id=ID', 'absolute=true') ?>";
