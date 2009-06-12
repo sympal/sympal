99c960295ef65e883d13dd913271ba33c89d9a24
jwage <jonwage@gmail.com>
2009-06-12 15:25:33 +0000

    [1.0] sfSympalPlugin: Fixes issue with save and add
    git-svn-id: http://svn.symfony-project.org/plugins/sfSympalPlugin/trunk@19201 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/plugins/sfSympalMenuPlugin/modules/sympal_menu_items/lib/Basesympal_menu_itemsActions.class.php
+++ lib/plugins/sfSympalMenuPlugin/modules/sympal_menu_items/lib/Basesympal_menu_itemsActions.class.php
@@ -259,26 +259,4 @@ class Basesympal_menu_itemsActions extends autoSympal_menu_itemsActions
     $this->form->setDefault('parent_id', $request->getParameter('id'));
     $this->setTemplate('edit');
   }
-
-  protected function processForm(sfWebRequest $request, sfForm $form)
-  {
-    $form->bind($request->getParameter($form->getName()), $request->getFiles($form->getName()));
-    if ($form->isValid())
-    {
-      $this->getUser()->setFlash('notice', $form->getObject()->isNew() ? 'The item was created successfully.' : 'The item was updated successfully.');
-
-      $menuItem = $form->save();
-
-      $this->dispatcher->notify(new sfEvent($this, 'admin.save_object', array('object' => $menuItem)));
-
-      if ($request->hasParameter('_save_and_add'))
-      {
-        $this->getUser()->setFlash('notice', $this->getUser()->getFlash('notice').' You can add another one below.');
-      }
-    }
-    else
-    {
-      $this->getUser()->setFlash('error', 'The item has not been saved due to some errors.');
-    }
-  }
 }
