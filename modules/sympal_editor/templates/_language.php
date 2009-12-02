23c40ebafe0446abb924342e2409c55783fa28f4
jwage <jonwage@gmail.com>
2009-12-02 05:54:42 +0000

    [1.4][sfSympalPlugin] Fixing invalid html
    git-svn-id: http://svn.symfony-project.org/plugins/sfSympalPlugin/trunk@24758 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/sympal_editor/templates/_language.php
+++ modules/sympal_editor/templates/_language.php
@@ -1,3 +1,6 @@
 <form action="<?php echo url_for('@sympal_change_language') ?>">
-  <?php echo $form ?><input type="submit" value="<?php echo __('ok') ?>" />
+  <table>
+    <?php echo $form ?>
+  </table>
+  <input type="submit" value="<?php echo __('ok') ?>" />
 </form>
