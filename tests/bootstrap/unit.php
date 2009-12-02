ca3ce58e948579db6a7963a072240c7ec4c5edae
jwage <jonwage@gmail.com>
2009-12-02 08:10:38 +0000

    [1.4][sfSympalPlugin] Removing autoload again
    git-svn-id: http://svn.symfony-project.org/plugins/sfSympalPlugin/trunk@24776 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- tests/bootstrap/unit.php
+++ tests/bootstrap/unit.php
@@ -27,13 +27,5 @@ if (isset($database) && $database)
 
 require_once $configuration->getSymfonyLibDir().'/vendor/lime/lime.php';
 
-function sympal_autoload_again($class)
-{
-  $autoload = sfSimpleAutoload::getInstance();
-  $autoload->reload();
-  return $autoload->autoload($class);
-}
-spl_autoload_register('sympal_autoload_again');
-
 require_once dirname(__FILE__).'/../../config/sfSympalPluginConfiguration.class.php';
 $plugin_configuration = new sfSympalPluginConfiguration($configuration, dirname(__FILE__).'/../..');
