c4d0e230bd432b11fd4e0d9c892d6ee66deb8f98
jwage <jonwage@gmail.com>
2009-12-07 22:31:08 +0000

    [1.4][sfSympalPlugin] Fixing issue with sympal_groups module not rendering with admin theme
    git-svn-id: http://svn.symfony-project.org/plugins/sfSympalPlugin/trunk@25055 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/plugins/sfSympalUserPlugin/modules/sympal_groups/actions/actions.class.php
+++ lib/plugins/sfSympalUserPlugin/modules/sympal_groups/actions/actions.class.php
@@ -10,6 +10,6 @@ require_once dirname(__FILE__).'/../lib/Basesympal_groupsActions.class.php';
  * @author     Your name here
  * @version    SVN: $Id: actions.class.php 12474 2008-10-31 10:41:27Z jwage $
  */
-class sympal_groupsActions extends autoSympal_groupsActions
+class sympal_groupsActions extends Basesympal_groupsActions
 {
 }
