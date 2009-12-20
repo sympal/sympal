4b7f97d3b7bb4b80d1bd19d9773fd95d635a5226
jwage <jonwage@gmail.com>
2009-12-20 05:47:27 +0000

    [1.4][sfSympalPlugin] Enhancing table_method to be a dropdown of available methods on the type table class
    git-svn-id: http://svn.symfony-project.org/plugins/sfSympalPlugin/trunk@25667 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/plugins/sfSympalContentListPlugin/lib/form/doctrine/PluginsfSympalContentListForm.class.php
+++ lib/plugins/sfSympalContentListPlugin/lib/form/doctrine/PluginsfSympalContentListForm.class.php
@@ -33,5 +33,32 @@ abstract class PluginsfSympalContentListForm extends BasesfSympalContentListForm
 
     $this->widgetSchema['sort_column'] = new sfWidgetFormChoice(array('choices' => $choices));
     $this->widgetSchema['sort_order'] = new sfWidgetFormChoice(array('choices' => array('' => '', 'ASC' => 'ASC', 'DESC' => 'DESC')));
+
+    $tableMethods = $this->_getTableMethods(array($type.'Table', 'Plugin'.$type.'Table'));
+
+    $this->widgetSchema['table_method'] = new sfWidgetFormChoice(array('choices' => $tableMethods));
+  }
+
+  private function _getTableMethods($classes)
+  {
+    if (is_array($classes))
+    {
+      $tableMethods = array();
+      foreach ($classes as $class)
+      {
+        $tableMethods = array_merge($tableMethods, $this->_getTableMethods($class));
+      }
+      return $tableMethods;
+    } else {
+      $class = $classes;
+      $array1 = get_class_methods($class);
+      if ($parentClass = get_parent_class($class)){
+        $array2 = get_class_methods($parentClass);
+        $array3 = array_diff($array1, $array2);
+      } else {
+        $array3 = $array1;
+      }
+      return $array3;
+    }
   }
 }
