788f2b94ee0c5f7a6feb9bbfa57740b1dbc9ff08
jwage <jonwage@gmail.com>
2009-12-20 14:14:37 +0000

    [1.4][sfSympalPlugin] Removing disable_comments integration with pages.
    git-svn-id: http://svn.symfony-project.org/plugins/sfSympalPlugin/trunk@25686 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/plugins/sfSympalPagesPlugin/modules/sympal_page/templates/_view.php
+++ lib/plugins/sfSympalPagesPlugin/modules/sympal_page/templates/_view.php
@@ -5,8 +5,3 @@
 <h1><?php echo get_sympal_column_content_slot($content, 'title') ?></h1>
 
 <?php echo get_sympal_content_slot($content, 'body', 'Markdown') ?>
-
-<?php if (!$record->disable_comments && sfSympalConfig::get('sfSympalCommentsPlugin', 'installed') && sfSympalConfig::get('sfSympalCommentsPlugin', 'enabled') && sfSympalConfig::get('sfSympalPage', 'enable_comments')): ?>
-  <?php use_helper('Comments') ?>
-  <?php echo get_sympal_comments($content) ?>
-<?php endif; ?>
