ff509b1c1c96e0ca0da816f94797cd8bf33464b4
jwage <jonwage@gmail.com>
2009-12-24 12:20:23 +0000

    [1.4][sfSympalPlugin][0.7] Removing unused files
    git-svn-id: http://svn.symfony-project.org/plugins/sfSympalPlugin/trunk@25871 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/sympal_default/templates/_render_form.php
+++
@@ -1,6 +0,0 @@
-<fieldset id="<?php echo $form->getName() ?>">
-  <?php foreach ($form as $name => $widget): ?>
-    <?php if ($widget->isHidden()) continue; ?>
-    <?php echo get_partial('sympal_default/render_form_widget', array('form' => $form, 'name' => $name, 'widget' => $widget)) ?>
-  <?php endforeach; ?>
-</fieldset>
