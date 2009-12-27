f4cccec0d4b24cd8f612e0c51e96f4e2212f9cf3
jwage <jonwage@gmail.com>
2009-12-27 15:51:25 +0000

    [1.4][sfSympalPlugin][0.7] Removing unused css from editor.css and fixing markdown css path
    git-svn-id: http://svn.symfony-project.org/plugins/sfSympalPlugin/trunk@25929 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/plugins/sfSympalRenderingPlugin/lib/sfSympalMarkdownRenderer.class.php
+++ lib/plugins/sfSympalRenderingPlugin/lib/sfSympalMarkdownRenderer.class.php
@@ -9,7 +9,7 @@ class sfSympalMarkdownRenderer
     $markdown = str_replace('{?php', '<?php', $markdown);
     $markdown = str_replace('?}', '?>', $markdown);
 
-    sfContext::getInstance()->getResponse()->addStylesheet('/sfSympalPlugin/css/markdown.css');
+    sfContext::getInstance()->getResponse()->addStylesheet('/sfSympalRenderingPlugin/css/markdown.css');
 
     return '<div class="sympal_markdown">'.self::enhanceHtml(Markdown($markdown)).'</div>';
   }
