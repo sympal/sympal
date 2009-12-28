705ac98ecc184ef86004bdd9fa5d447f7025f290
jwage <jonwage@gmail.com>
2009-12-28 01:17:28 +0000

    [1.4][sfSympalPlugin][0.7] Refactoring code to manage the enabling of Sympal plugins to allow more flexibility with which plugins the user wants to enable
    git-svn-id: http://svn.symfony-project.org/plugins/sfSympalPlugin/trunk@25940 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- config/sfSympalPluginConfiguration.class.php
+++ config/sfSympalPluginConfiguration.class.php
@@ -28,98 +28,19 @@ class sfSympalPluginConfiguration extends sfPluginConfiguration
   public
     $sympalConfiguration;
 
-  /**
-   * Check Whether or not Sympal is enabled for an application
-   *
-   * @return boolean $enabled
-   */
-  public static function isSympalEnabled($application = null)
-  {
-    if (!$application)
-    {
-      $application = sfConfig::get('sf_app');
-    }
-    if ($application)
-    {
-      $reflection = new ReflectionClass($application.'Configuration');
-      if ($reflection->getConstant('disableSympal'))
-      {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  /**
-   * Enable only the Sympal plugins required for the frontend.
-   *
-   * @param ProjectConfiguration $configuration
-   */
-  public static function enableFrontendPlugins(ProjectConfiguration $configuration)
-  {
-    if (!self::isSympalEnabled())
-    {
-      return false;
-    }
-
-    $configuration->enablePlugins('sfDoctrinePlugin');
-    $configuration->enablePlugins('sfSympalPlugin');
-    $configuration->setPluginPath('sfSympalPlugin', dirname(dirname(__FILE__)));
-
-    self::enableSympalCorePlugins($configuration, array(
-      'sfDoctrineGuardPlugin',
-      'sfFeed2Plugin',
-      'sfWebBrowserPlugin',
-      'sfSympalMenuPlugin',
-      'sfSympalPagesPlugin',
-      'sfSympalContentListPlugin',
-      'sfSympalDataGridPlugin',
-      'sfSympalUserPlugin',
-      'sfSympalRenderingPlugin',
-    ));
-  }
-
-  /**
-   * Enable all Sympal plugins always
-   *
-   * @param ProjectConfiguration $configuration 
-   * @return void
-   */
-  public static function enableSympalPlugins(ProjectConfiguration $configuration)
-  {
-    if (!self::isSympalEnabled())
-    {
-      return false;
-    }
-
-    $configuration->enablePlugins('sfDoctrinePlugin');
-    $configuration->enablePlugins('sfSympalPlugin');
-    $configuration->setPluginPath('sfSympalPlugin', dirname(dirname(__FILE__)));
-
-    self::enableSympalCorePlugins($configuration, self::$dependencies);
-  }
-
-  /**
-   * Enable some Sympal core plugins
-   *
-   * @param ProjectConfiguration $configuration 
-   * @param array|string $plugins
-   * @return void
-   */
-  public static function enableSympalCorePlugins(ProjectConfiguration $configuration, $plugins)
-  {
-    foreach ((array) $plugins as $plugin)
-    {
-      $configuration->enablePlugins($plugin);
-      $configuration->setPluginPath($plugin, dirname(dirname(__FILE__)).'/lib/plugins/'.$plugin);
-    }
-  }
-
   public function initialize()
   {
     $this->sympalConfiguration = new sfSympalConfiguration($this->dispatcher, $this->configuration);
 
     $this->dispatcher->connect('form.post_configure', array($this, 'formPostConfigure'));
+  }
+
+  public static function enableSympalPlugins(ProjectConfiguration $configuration)
+  {
+    require_once(dirname(__FILE__).'/../lib/core/sfSympalPluginEnabler.class.php');
+
+    $enabler = new sfSympalPluginEnabler($configuration);
+    $enabler->enableSympalPlugins();
   }
 
   public function getSympalConfiguration()
---
+++ lib/core/sfSympalPluginEnabler.class.php
@@ -0,0 +1,89 @@
+<?php
+
+class sfSympalPluginEnabler
+{
+  private
+    $_configuration,
+    $_isSympalEnabled = null,
+    $_sympalPluginPath = null;
+
+  public function __construct(ProjectConfiguration $configuration)
+  {
+    $this->_configuration = $configuration;
+    $this->_sympalPluginPath = realpath(dirname(__FILE__).'/../..');
+  }
+
+  public function isSympalEnabled()
+  {
+    if ($this->_isSympalEnabled === null)
+    {
+      $this->_isSympalEnabled = true;
+      if ($application = sfConfig::get('sf_app'))
+      {
+        $reflection = new ReflectionClass($application.'Configuration');
+        if ($reflection->getConstant('disableSympal'))
+        {
+          $this->_isSympalEnabled = false;
+        }
+      }
+    }
+    return $this->_isSympalEnabled;
+  }
+
+  public function enableSympalPlugins()
+  {
+    if (!$this->isSympalEnabled())
+    {
+      return false;
+    }
+
+    $this->_configuration->enablePlugins('sfDoctrinePlugin');
+    $this->_configuration->enablePlugins('sfSympalPlugin');
+    $this->_configuration->setPluginPath('sfSympalPlugin', $this->_sympalPluginPath);
+
+    $this->enableSympalCorePlugins(sfSympalPluginConfiguration::$dependencies);
+  }
+
+  public function enableFrontendPlugins()
+  {
+    if (!$this->isSympalEnabled())
+    {
+      return false;
+    }
+
+    $this->_configuration->enablePlugins('sfDoctrinePlugin');
+    $this->_configuration->enablePlugins('sfSympalPlugin');
+    $this->_configuration->setPluginPath('sfSympalPlugin', $this->_sympalPluginPath);
+
+    $this->enableSympalCorePlugins(array(
+      'sfDoctrineGuardPlugin',
+      'sfFeed2Plugin',
+      'sfWebBrowserPlugin',
+      'sfSympalMenuPlugin',
+      'sfSympalPagesPlugin',
+      'sfSympalContentListPlugin',
+      'sfSympalDataGridPlugin',
+      'sfSympalUserPlugin',
+      'sfSympalRenderingPlugin',
+    ));
+  }
+
+  public function enableSympalCorePlugins($plugins)
+  {
+    foreach ((array) $plugins as $plugin)
+    {
+      $this->_configuration->enablePlugins($plugin);
+      $this->_configuration->setPluginPath($plugin, $this->_sympalPluginPath.'/lib/plugins/'.$plugin);
+    }
+  }
+
+  public function overrideSympalPlugin($plugin, $newPlugin, $newPluginPath = null)
+  {
+    $this->_configuration->disablePlugin($plugin);
+    $this->_configuration->enablePlugins($newPlugin);
+    if ($newPluginPath)
+    {
+      $this->_configuration->setPluginPath($newPlugin, $newPluginPath);
+    }
+  }
+}
