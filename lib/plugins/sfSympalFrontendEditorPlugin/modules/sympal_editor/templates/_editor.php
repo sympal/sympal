3fa069cde5d4f8fa068e302ef2995ce6eb256377
jwage <jonwage@gmail.com>
2009-12-31 07:55:53 +0000

    [1.4][sfSympalPlugin][1.0] Fixing change language dropdown in inline edit bar
    git-svn-id: http://svn.symfony-project.org/plugins/sfSympalPlugin/trunk@26073 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/plugins/sfSympalFrontendEditorPlugin/modules/sympal_editor/templates/_editor.php
+++ lib/plugins/sfSympalFrontendEditorPlugin/modules/sympal_editor/templates/_editor.php
@@ -8,6 +8,21 @@
   <div class="sympal_inline_edit_bar_container">
     <ul>
       <li><?php echo button_to('X', '@sympal_signout', 'title=Signout class=signout confirm=Are you sure you want to signout?') ?></li>
+      <?php if (sfSympalConfig::isI18nEnabled()): ?>
+        <li>
+          <?php
+          $user = sfContext::getInstance()->getUser();
+          $form = new sfFormLanguage($user, array('languages' => sfSympalConfig::get('language_codes', null, array($user->getCulture()))));
+          unset($form[$form->getCSRFFieldName()]);
+          $widgetSchema = $form->getWidgetSchema();
+          $widgetSchema['language']->setAttribute('onChange', "this.form.submit();");
+          ?>
+
+          <?php echo $form->renderFormTag(url_for('@sympal_change_language_form')) ?>
+            <?php echo $form['language'] ?>
+          </form>
+        </li>
+      <?php endif; ?>
       <li><input type="button" class="toggle_editor_menu" name="toggle_editor_menu" value="Editor Menu" title="Click to toggle Sympal editor menu" /></li>
       <li><?php echo button_to('Dashboard', '@sympal_dashboard', array('class' => 'sympal_dashboard')) ?></li>
       <li><input type="button" class="toggle_edit_mode" value="Enable Edit Mode" /></li>
@@ -21,22 +36,6 @@
       <li><input type="button" class="sympal_disable_edit_mode" name="disable_edit_mode" value="Disable Edit Mode" /></li>
     </ul>
   </div>
-
-  <?php if (sfSympalConfig::isI18nEnabled()): ?>
-    <div class="sympal_inline_edit_bar_change_language">
-      <?php
-      $user = sfContext::getInstance()->getUser();
-      $form = new sfFormLanguage($user, array('languages' => sfSympalConfig::get('language_codes', null, array($user->getCulture()))));
-      unset($form[$form->getCSRFFieldName()]);
-      $widgetSchema = $form->getWidgetSchema();
-      $widgetSchema['language']->setAttribute('onChange', "this.form.submit();");
-      ?>
-  
-      <?php echo $form->renderFormTag(url_for('@sympal_change_language_form')) ?>
-        <?php echo $form['language'] ?>
-      </form>
-    </div>
-  <?php endif; ?>
 </div>
 
 <div id="sympal_assets"></div>
