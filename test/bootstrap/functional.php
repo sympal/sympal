4810805ec043bdba9666dd4a9b3859cb93caf683
jwage <jonwage@gmail.com>
2010-01-04 07:49:26 +0000

    [1.4][sfSympalPlugin][1.0] Fix for sismo tests
    git-svn-id: http://svn.symfony-project.org/plugins/sfSympalPlugin/trunk@26161 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- test/bootstrap/functional.php
+++ test/bootstrap/functional.php
@@ -9,6 +9,7 @@ require_once dirname(__FILE__).'/../fixtures/project/config/ProjectConfiguration
 $configuration = ProjectConfiguration::getApplicationConfiguration($app, 'test', isset($debug) ? $debug : true);
 
 require_once(dirname(__FILE__).'/cleanup.php');
+require_once(sfConfig::get('sf_lib_dir').'/sfSympalTestFunctional.class.php');
 
 sfContext::createInstance($configuration);
 
