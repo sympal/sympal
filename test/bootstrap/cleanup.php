ce0aa64d0353bd7df0261266c33869e918d9273f
jwage <jonwage@gmail.com>
2010-01-04 07:27:35 +0000

    [1.4][sfSympalPlugin][1.0] Fix for sismo tests
    git-svn-id: http://svn.symfony-project.org/plugins/sfSympalPlugin/trunk@26160 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- test/bootstrap/cleanup.php
+++ test/bootstrap/cleanup.php
@@ -5,9 +5,9 @@ function sympal_cleanup()
   sfToolkit::clearDirectory(dirname(__FILE__).'/../fixtures/project/cache');
   sfToolkit::clearDirectory(dirname(__FILE__).'/../fixtures/project/log');
 
-  @unlink(dirname(__FILE__) . '/../fixtures/project/data/test.sqlite');
+  copy(dirname(__FILE__).'/../fixtures/project/data/fresh_test_db.sqlite', dirname(__FILE__).'/../fixtures/project/data/test.sqlite');
   @unlink(sfConfig::get('sf_config_dir').'/app.yml');
 }
 
-copy(dirname(__FILE__).'/../fixtures/project/data/fresh_test_db.sqlite', dirname(__FILE__).'/../fixtures/project/data/test.sqlite');
+sympal_cleanup();
 register_shutdown_function('sympal_cleanup');
