929481fc793c6b3fd2504b26715f0f410580807a
jwage <jonwage@gmail.com>
2010-01-05 22:38:52 +0000

    [1.4][sfSympalPlugin][1.0] Showing hierarchy of content when viewing content list
    git-svn-id: http://svn.symfony-project.org/plugins/sfSympalPlugin/trunk@26250 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/plugins/sfSympalAdminPlugin/modules/sympal_content/templates/_title.php
+++ lib/plugins/sfSympalAdminPlugin/modules/sympal_content/templates/_title.php
@@ -1,2 +1,9 @@
-<?php echo link_to($sf_sympal_content, $sf_sympal_content->getRoute()) ?><br/>
-<small><?php echo $sf_sympal_content->getEvaluatedRoutePath() ?></small>
+<?php $padding ?>
+<?php if ($menuItem = $sf_sympal_content->getMenuItem()): ?>
+  <?php echo $padding = str_repeat(' &nbsp; &nbsp; ', $menuItem->getLevel()) ?>
+  <?php echo image_tag('/sfSympalPlugin/images/'.($menuItem->getNode()->isLeaf() ? 'page' : 'folder').'.png') ?>
+<?php endif; ?>
+
+<?php echo link_to($sf_sympal_content, $sf_sympal_content->getEditRoute()) ?>
+
+<br/><?php echo $padding ?> &nbsp; &nbsp; &nbsp;<small><?php echo link_to($sf_sympal_content->getEvaluatedRoutePath(), $sf_sympal_content->getRoute()) ?></small>
