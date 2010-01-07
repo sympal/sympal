4b695269d2aa375452b37a125d537410018a1660
jwage <jonwage@gmail.com>
2010-01-07 01:28:54 +0000

    [1.4][sfSympalPlugin][1.0] Fixing theme for new sites
    git-svn-id: http://svn.symfony-project.org/plugins/sfSympalPlugin/trunk@26324 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/sympal_default/lib/Basesympal_defaultActions.class.php
+++ modules/sympal_default/lib/Basesympal_defaultActions.class.php
@@ -4,7 +4,7 @@ class Basesympal_defaultActions extends sfActions
 {
   public function executeNew_site(sfWebRequest $request)
   {
-    $this->useDefaultTheme();
+    $this->loadTheme($this->getSympalContext()->getSite()->getTheme());
   }
 
   public function executeChange_language(sfWebRequest $request)
