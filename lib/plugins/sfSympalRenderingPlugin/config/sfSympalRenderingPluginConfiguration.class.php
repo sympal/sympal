edcc4a05d7be80f3fa5195b0edec7ad6f952e17b
jwage <jonwage@gmail.com>
2010-01-09 06:09:57 +0000

    [1.4][sfSympalPlugin][1.0] Adding ability to easily integrate with google analytics
    git-svn-id: http://svn.symfony-project.org/plugins/sfSympalPlugin/trunk@26420 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/plugins/sfSympalRenderingPlugin/config/sfSympalRenderingPluginConfiguration.class.php
+++ lib/plugins/sfSympalRenderingPlugin/config/sfSympalRenderingPluginConfiguration.class.php
@@ -2,4 +2,24 @@
 
 class sfSympalRenderingPluginConfiguration extends sfPluginConfiguration
 {
+  public function initialize()
+  {
+    $this->dispatcher->connect('response.filter_content', array($this, 'listenToResponseFilterContent'));
+  }
+
+  public function listenToResponseFilterContent(sfEvent $event, $content)
+  {
+    if ($code = sfSympalConfig::get('google_analytics_code'))
+    {
+      $js = '<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+</script>
+<script type="text/javascript">
+_uacct = "'.$code.'";
+urchinTracker();
+</script>';
+      return str_replace('</body>', $js.'</body>', $content);
+    } else {
+      return $content;
+    }
+  }
 }
