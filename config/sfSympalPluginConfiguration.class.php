939083684ac5d117770d41105844bb2efbb2cd30
jwage <jonwage@gmail.com>
2010-01-15 00:33:26 +0000

    [1.4][sfSympalPlugin][1.0] Changing version to 1.0.0-ALPHA1
    git-svn-id: http://svn.symfony-project.org/plugins/sfSympalPlugin/trunk@26662 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- config/sfSympalPluginConfiguration.class.php
+++ config/sfSympalPluginConfiguration.class.php
@@ -2,7 +2,7 @@
 
 class sfSympalPluginConfiguration extends sfPluginConfiguration
 {
-  const VERSION = '1.0.0-DEV';
+  const VERSION = '1.0.0-ALPHA1';
 
   public static
     $dependencies = array(
