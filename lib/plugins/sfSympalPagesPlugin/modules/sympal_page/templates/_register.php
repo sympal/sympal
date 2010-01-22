c04861d1662b3bb941fb0989ce6debf1d00f068c
jwage <jonwage@gmail.com>
2010-01-22 07:48:17 +0000

    [1.4][sfSympalPlugin][1.0] Added some new french translations
    git-svn-id: http://svn.symfony-project.org/plugins/sfSympalPlugin/trunk@27030 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/plugins/sfSympalPagesPlugin/modules/sympal_page/templates/_register.php
+++ lib/plugins/sfSympalPagesPlugin/modules/sympal_page/templates/_register.php
@@ -1,7 +1,7 @@
 <?php echo get_sympal_breadcrumbs($menuItem, $content) ?>
 
 <?php if ($sf_user->isAuthenticated()): ?>
-  <p>You are already registered.</p>
+  <p><?php echo __('You are already registered.'); ?></p>
 <?php else: ?>
   <h1><?php echo get_sympal_content_slot($content, 'title') ?></h1>
 
