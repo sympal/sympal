73eeb9796c9706e18821d3dc3a651f50d44b1397
jwage <jonwage@gmail.com>
2010-01-23 06:49:00 +0000

    [1.4][sfSympalPlugin][1.0] updating the Comments test for new changes made to the comments plugin. Also, moved the app.yml cleanup to before the bootstrap - after the bootstrap it seemed to cause inconsistent loads - some test runs ran differently than others. The test db was updated for the new columns for the comments model.
    git-svn-id: http://svn.symfony-project.org/plugins/sfSympalPlugin/trunk@27085 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- test/bootstrap/functional.php
+++ test/bootstrap/functional.php
@@ -5,10 +5,11 @@ if (!isset($app))
   $app = 'sympal';
 }
 
+require_once(dirname(__FILE__).'/cleanup.php');
+
 require_once dirname(__FILE__).'/../fixtures/project/config/ProjectConfiguration.class.php';
 $configuration = ProjectConfiguration::getApplicationConfiguration($app, 'test', isset($debug) ? $debug : true);
 
-require_once(dirname(__FILE__).'/cleanup.php');
 require_once(sfConfig::get('sf_lib_dir').'/sfSympalTestFunctional.class.php');
 
 sfContext::createInstance($configuration);
--- test/functional/CommentsTest.php
+++ test/functional/CommentsTest.php
@@ -1,27 +1,126 @@
 <?php
 
+/**
+ * Functional testing for sfSympalCommentsPlugin
+ * 
+ * @author      Jon Wage <jonwage@gmail.com>
+ * @author      Ryan Weaver <ryan@thatsquality.com>
+ * @since       2010-01-22
+ * @version     svn:$Id$ $Author$
+ */
 require_once(dirname(__FILE__).'/../bootstrap/functional.php');
 
 $browser = new sfSympalTestFunctional(new sfBrowser());
+$browser->setTester('doctrine', 'sfTesterDoctrine');
 
-$browser->
+$goodValues = array(
+  'website' => 'http://www.sympalphp.org',
+  'body'    => 'Hey, nice test comment!',
+);
+$browser->info('1 - Sign in and post some comments')->
   signInAsAdmin()->
+  
   get('/blog-post/sample_blogpost')->
+  
+  with('response')->begin()->
+    isStatusCode(200)->
+    checkElement('#sympal_comments', true)->
+    checkForm('sfSympalNewCommentForm')->
+  end()->
+  
+  info('  1.1 - Submit a bad form, count the errors')->
+  
   click('Save Comment', array('sf_sympal_comment' => array(
-    'subject' => 'Test',
-    'body' => 'Test',
-  )), array('_with_csrf' => true))
+    'website' => 'bad site',
+  )))->
+  
+  with('form')->begin()->
+    hasErrors(2)->
+    isError('body', 'required')->
+    isError('website', 'invalid')->
+  end()->
+  
+  click('Save Comment', array('sf_sympal_comment' => $goodValues))->
+  
+  with('form')->begin()->
+    hasErrors(0)->
+  end()->
+  
+  with('request')->begin()->
+    isParameter('module', 'sympal_comments')->
+    isParameter('action', 'create')->
+  end()
+;
+
+Doctrine_Core::getTable('sfSympalBlogPost')->getConnection()->clear();
+
+$sympalAdmin = Doctrine_Core::getTable('sfGuardUser')->findOneByUsername('admin');
+$browser->
+  with('response')->isRedirected()->followRedirect()->
+  
+  with('response')->begin()->
+    matches('/comment_1/')->
+    matches('/Posted on (.*) by/')->
+    info('  1.2 - Check that the website is surrounded by a nofollow')->
+    checkElement('a[rel="nofollow"][href="'.$goodValues['website'].'"]', '/Sympal Admin/')->
+  end()->
+  
+  with('doctrine')->begin()->
+    check('sfSympalComment', array_merge(
+      $goodValues,
+      array('user_id' => $sympalAdmin->id)
+    ))->
+  end()
+;
+
+$goodValues = array(
+  'email_address' => 'ryan@thatsquality.com',
+  'body'  => 'just another test comment',
+);
+$browser->info('2 - Post some comments anonymously')->
+  signOut()->
+  
+  get('/blog-post/sample_blogpost')->
+  
+  with('response')->begin()->
+    isStatusCode(200)->
+    checkElement('#sympal_comments', true)->
+    checkForm('sfSympalNewCommentForm')->
+  end()->
+  
+  info('  2.1 - Submit a bad form, count the errors')->
+  
+  click('Save Comment')->
+  
+  with('form')->begin()->
+    hasErrors(2)->
+    isError('body', 'required')->
+    isError('email_address', 'required')->
+  end()->
+  
+  click('Save Comment', array('sf_sympal_comment' => $goodValues))->
+  
+  with('form')->begin()->
+    hasErrors(0)->
+  end()->
+  
+  with('request')->begin()->
+    isParameter('module', 'sympal_comments')->
+    isParameter('action', 'create')->
+  end()
 ;
 
 Doctrine_Core::getTable('sfSympalBlogPost')->getConnection()->clear();
 
 $browser->
+  with('response')->isRedirected()->followRedirect()->
+  
   with('response')->begin()->
-    isRedirected()->
-    followRedirect()->
+    matches('/comment_2/')->
+    matches('/Posted on (.*) by[\s]+ anonymous\./')->
   end()->
-  with('response')->begin()->
-    matches('/comment_1/')->
-    matches('/Posted on (.*) by Sympal Admin./')->
+  
+  with('doctrine')->begin()->
+    check('sfSympalComment', $goodValues)->
   end()
 ;
