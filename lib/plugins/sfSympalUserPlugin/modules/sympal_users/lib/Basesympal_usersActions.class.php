8f8e30db913c37ed08132c82fc6f3ed8d7f90eb5
jwage <jonwage@gmail.com>
2010-02-10 16:44:24 +0000

    [1.4][sfSympalPlugin][1.0] Disallowing the deleting of yourself as a logged in user to avoid locking yourself out of Sympal
    git-svn-id: http://svn.symfony-project.org/plugins/sfSympalPlugin/trunk@27829 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/plugins/sfSympalUserPlugin/modules/sympal_users/lib/Basesympal_usersActions.class.php
+++ lib/plugins/sfSympalUserPlugin/modules/sympal_users/lib/Basesympal_usersActions.class.php
@@ -5,4 +5,19 @@ require_once dirname(__FILE__).'/../lib/sympal_usersGeneratorHelper.class.php';
 
 class Basesympal_usersActions extends autosympal_usersActions
 {
+  public function preExecute()
+  {
+    parent::preExecute();
+    
+    $this->getContext()->getEventDispatcher()->connect('admin.delete_object', array($this, 'listenToAdminDeleteObject'));
+  }
+
+  public function listenToAdminDeleteObject(sfEvent $event)
+  {
+    if ($this->getUser()->getGuardUser()->getId() == $event['object']->getId())
+    {
+      $this->getUser()->setFlash('error', 'You cannot delete yourself from the database!');
+      $this->redirect('@sympal_users');
+    }
+  }
 }
