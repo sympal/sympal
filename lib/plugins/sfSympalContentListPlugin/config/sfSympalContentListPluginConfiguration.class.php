0507ece8a25088b18e7eb542314a92bf2423bfa6
jwage <jonwage@gmail.com>
2010-02-17 01:22:22 +0000

    [1.4][sfSympalPlugin][1.0] Reverting more changes
    git-svn-id: http://svn.symfony-project.org/plugins/sfSympalPlugin/trunk@28069 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/plugins/sfSympalContentListPlugin/config/sfSympalContentListPluginConfiguration.class.php
+++ lib/plugins/sfSympalContentListPlugin/config/sfSympalContentListPluginConfiguration.class.php
@@ -2,7 +2,7 @@
 
 /**
  * sfSympalContentListPlugin configuration.
- *
+ * 
  * @package     sfSympalContentListPlugin
  * @subpackage  config
  * @author      Your name here
@@ -10,15 +10,66 @@
  */
 class sfSympalContentListPluginConfiguration extends sfPluginConfiguration
 {
-  const VERSION = '1.0.0-ALPHA3';
+  const VERSION = '1.0.0-DEV';
 
   /**
    * @see sfPluginConfiguration
    */
   public function initialize()
   {
-    new sfSympalContentListFilterVariablesListener( $this->dispatcher );
-    new sfSympalContentListUnknownFormatListener(   $this->dispatcher );
+    $this->dispatcher->connect('sympal.content_renderer.filter_variables', array($this, 'listenForFilterVariables'));
+    $this->dispatcher->connect('sympal.content_renderer.unknown_format', array($this, 'listenForUnknownFormat'));
   }
 
+  public function listenForFilterVariables(sfEvent $event, $variables)
+  {
+    $content = $variables['content'];
+    if ($content->getType()->getName() == 'sfSympalContentList')
+    {
+      $contentList = $content->getRecord();
+
+      $request = sfContext::getInstance()->getRequest();
+      $dataGrid = $contentList->buildDataGrid($request);
+      $pager = $dataGrid->getPager();
+
+      $variables['pager'] = $pager;
+      $variables['dataGrid'] = $dataGrid;
+    }
+
+    return $variables;
+  }
+
+  public function listenForUnknownFormat(sfEvent $event)
+  {
+    if (isset($event['pager']))
+    {
+      $pager = $event['pager'];
+      $context = sfContext::getInstance();
+      $response = $context->getResponse();
+      $request = $context->getRequest();
+
+      $className = 'sf'.ucfirst($event['format']).'Feed';
+      if (!class_exists($className))
+      {
+        return false;
+      }
+
+      $feed = sfFeedPeer::newInstance($event['format']);
+
+      $feed->initialize(array(
+        'title'       => $response->getTitle(),
+        'link'        => $request->getUri()
+      ));
+
+      $items = sfFeedPeer::convertObjectsToItems($pager->getResults());
+      $feed->addItems($items);
+
+      $event->setProcessed(true);
+      $event->setReturnValue($feed->asXml());
+
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
