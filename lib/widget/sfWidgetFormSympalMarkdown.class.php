18c051cea61649b0482edd36608d63eab8a7cc0b
Ryan Weaver <ryan@thatsquality.com>
2010-02-25 07:30:00 -0600

    [1.4][sfSympalPlugin][1.0] Making the markdown plugin proper. It now outputs its own setup javascript and includes the css and js that it needs.
    The inclusion of the stylesheets and javascript were removed from the form - they were placed there by mistake.

--- lib/widget/sfWidgetFormSympalMarkdown.class.php
+++ lib/widget/sfWidgetFormSympalMarkdown.class.php
@@ -1,5 +1,54 @@
 <?php
 
+/**
+ * Special widget for rendering markdown textareas
+ * 
+ * @package     sfSympalPlugin
+ * @subpackage  widget
+ * @author      Ryan Weaver <ryan@thatsquality.com>
+ * @since       2010-02-24
+ * @version     svn:$Id$ $Author$
+ */
+
 class sfWidgetFormSympalMarkdown extends sfWidgetFormTextarea
 {
+  /**
+   * Overridden to add the necessary javascript for creating the markdown
+   * editor.
+   * 
+   * @see sfWidgetFormTextarea
+   */
+  public function render($name, $value = null, $attributes = array(), $errors = array())
+  {
+    $widget = parent::render($name, $value, $attributes, $errors);
+    
+    $id = $this->generateId($name, $value);
+    
+    $javascript = sprintf("
+<script type=\"text/javascript\">
+  $(document).ready(function(){
+    jQuery('#%s').markItUp(mySettings);
+  });
+</script>
+    ",
+    $id);
+    
+    return $widget.$javascript;
+  }
+  
+  public function getStylesheets()
+  {
+    return array(
+      '/sfSympalPlugin/markitup/skins/markitup/style.css' => '',
+      '/sfSympalPlugin/markitup/sets/markdown/style.css' => '',
+    );
+  }
+  
+  public function getJavascripts()
+  {
+    return array(
+      '/sfSympalPlugin/markitup/jquery.markitup.js',
+      '/sfSympalPlugin/markitup/sets/markdown/set.js',
+    );
+  }
 }
