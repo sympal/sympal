a7751f63be9c0b231f680e81432f437ae6b276f8
nippo <ippolito.nicolas@gmail.com>
2010-03-07 17:52:14 +0100

    [1.4][sfSympalPlugin] I propose to put Publis/Unpublih button in 'MyContent Actions' menu

--- lib/plugins/sfSympalAdminPlugin/config/sfSympalAdminPluginConfiguration.class.php
+++ lib/plugins/sfSympalAdminPlugin/config/sfSympalAdminPluginConfiguration.class.php
@@ -241,9 +241,29 @@ class sfSympalAdminPluginConfiguration extends sfPluginConfiguration
       {
         if (sfContext::getInstance()->getUser()->getEditCulture() != $code)
         {
-          $contentEditor->addChild(__('Edit ').format_language($code), '@sympal_change_edit_language?language='.$code, 'title=Switch to '.format_language($code));
+          $contentEditor->addChild(__('Edit ').format_language($code), '@sympal_change_edit_language?language='.$code, 'title='.__('Switch to ').''.format_language($code));
         }
       }
     }
+
+    if($user->hasCredential('PublishContent'))
+    {
+      if($content->getIsPublished())
+      {
+        $contentEditor
+          ->addChild(__('Unpublish'), '@sympal_unpublish_content?id='.$content['id'], 'title='.__('Published on %date%', array('%date%' => format_date($content->getDatePublished(), 'g'))).'. '.__('Click to unpublish content.'));
+      }
+      elseif($content->getIsPublishInTheFuture())
+      {
+        $contentEditor
+          ->addChild(__('Unpublish'), '@sympal_unpublish_content?id='.$content['id'], 'title='.__('Will publish on %date%', array('%date%' => format_date($content->getDatePublished(), 'g'))).'. '.__('Click to unpublish content.'));
+      }
+      else
+      {
+        $contentEditor
+          ->addChild(__('Publish'), '@sympal_publish_content?id='.$content['id'], 'title='.__('Has not been published yet.'.__('Click to publish content.')));
+      }
+    } 
+  
   }
 }
