be5459453efa6dae1b2cfb6d99f82d7e62d78d98
nippo <ippolito.nicolas@gmail.com>
2010-03-07 14:51:42 +0100

    [1.4][sfSympalPlugin] Translation

--- lib/plugins/sfSympalAdminPlugin/config/sfSympalAdminPluginConfiguration.class.php
+++ lib/plugins/sfSympalAdminPlugin/config/sfSympalAdminPluginConfiguration.class.php
@@ -75,7 +75,7 @@ class sfSympalAdminPluginConfiguration extends sfPluginConfiguration
         }
         if ($formatted)
         {
-          $changeLanguage->addChild($formatted, '@sympal_change_language?language='.$code, 'title=Switch to '.$formatted);
+          $changeLanguage->addChild(ucwords($formatted), '@sympal_change_language?language='.$code, 'title='.__('Switch to ').''.$formatted);
         }
       }
     }
