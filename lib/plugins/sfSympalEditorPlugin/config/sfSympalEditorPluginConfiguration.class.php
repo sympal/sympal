fef84c97e697605e3a7a1bd67fa346fdaa44839d
nippo <ippolito.nicolas@gmail.com>
2010-03-07 14:34:59 +0100

    [1.4][sfSympalPlugin] Just a change from Cancel to Quit for the sympal_edit_bar

--- lib/plugins/sfSympalEditorPlugin/config/sfSympalEditorPluginConfiguration.class.php
+++ lib/plugins/sfSympalEditorPlugin/config/sfSympalEditorPluginConfiguration.class.php
@@ -38,7 +38,7 @@ class sfSympalEditorPluginConfiguration extends sfPluginConfiguration
     ;
     
     $menu->
-      addChild('Cancel')->
+      addChild('Quit')->
       isEditModeButton(true)->
       setShortcut('Ctrl+Shift+X')->
       setInputClass('sympal_disable_edit_mode')
