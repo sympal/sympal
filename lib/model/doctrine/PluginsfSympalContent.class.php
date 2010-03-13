497cf31ffd9eb1f99b192505b5cd82065189ca0f
Ryan Weaver <ryan@thatsquality.com>
2010-03-12 22:38:52 -0600

    [1.4][sfSympalPlugin][1.0] Throwing an exception if someone tries to create a non-column slot to type Column

--- lib/model/doctrine/PluginsfSympalContent.class.php
+++ lib/model/doctrine/PluginsfSympalContent.class.php
@@ -182,6 +182,11 @@ abstract class PluginsfSympalContent extends BasesfSympalContent
     {
       $isColumn = $this->hasField($name) ? true : false;
       $type = $type ? $type : 'Text';
+      
+      if (!$isColumn && $type == 'Column')
+      {
+        throw new sfException('Cannot set a non-column slot to type "Column"');
+      }
 
       $slot = new sfSympalContentSlot();
       $slot->setContentRenderedFor($this);
