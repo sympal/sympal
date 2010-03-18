95ece092cc460fc34f6972fd626aa7d126ac071c
Ryan Weaver <ryan@thatsquality.com>
2010-03-18 06:52:55 -0500

    [1.4][sfSympalPlugin][1.0] Adding docblock to cache clear event.

--- lib/events/sfSympalTaskClearCacheListener.class.php
+++ lib/events/sfSympalTaskClearCacheListener.class.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * Adds a listener on the task.cache.clear to clear the combiner css
+ * and js cache in the web/cache directory
+ * 
+ * @package     sfSympalPlugin
+ * @subpackage  listener
+ * @author      Jonathan H. Wage <jonwage@gmail.com>
+ * @author      Ryan Weaver <ryan@thatsquality.com>
+ * @since       2010-03-18
+ * @version     svn:$Id$ $Author$
+ */
 class sfSympalTaskClearCacheListener extends sfSympalListener
 {
   /**
