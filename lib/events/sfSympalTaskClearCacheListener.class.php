f5826caa907c30e6e844b829794a39aec8da3a41
Ryan Weaver <ryan@thatsquality.com>
2010-03-18 06:51:31 -0500

    [1.4][sfSympalPlugin][1.0] Making our web/css cache clear report more gracefully on failures.

--- lib/events/sfSympalTaskClearCacheListener.class.php
+++ lib/events/sfSympalTaskClearCacheListener.class.php
@@ -2,6 +2,13 @@
 
 class sfSympalTaskClearCacheListener extends sfSympalListener
 {
+  /**
+   * A boolean for if this process has been run yet, since the task.cache.clear
+   * event will be called multiple times (once per environment) on a cache
+   * clear (and this task only needs to be called once)
+   */
+  protected static $_isProcessed = false;
+  
   public function getEventName()
   {
     return 'task.cache.clear';
@@ -9,12 +16,38 @@ class sfSympalTaskClearCacheListener extends sfSympalListener
 
   public function run(sfEvent $event)
   {
+    if (self::$_isProcessed)
+    {
+      return;
+    }
+    
     $event->getSubject()->logSection('sympal', 'Clearing web cache folder');
 
+    $failures = array();
     $cacheDir = sfConfig::get('sf_web_dir').'/cache';
     if (is_dir($cacheDir))
     {
-      $event->getSubject()->getFilesystem()->remove(sfFinder::type('file')->ignore_version_control()->discard('.sf')->in($cacheDir));
+      $filesystem = $event->getSubject()->getFilesystem();
+      
+      $finder = sfFinder::type('file')->ignore_version_control()->discard('.sf');
+      foreach ($finder->in($cacheDir) as $file)
+      {
+        @$filesystem->remove($file);
+        
+        if (file_exists($file))
+        {
+          $failures[] = $file;
+        }
+      }
+    }
+    self::$_isProcessed = true;
+    
+    if (count($failures) > 0)
+    {
+      $event->getSubject()->logBlock(array_merge(
+        array('Could not clear cache on the following files:', ''),
+        array_map(create_function('$f', 'return \' - \'.sfDebug::shortenFilePath($f);'), $failures)
+      ), 'ERROR_LARGE');
     }
   }
 }
