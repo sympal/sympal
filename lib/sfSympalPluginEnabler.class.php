a7037a35e053269fd38fe5a4146cf8cee6d2b154
Ryan Weaver <ryan@thatsquality.com>
2010-03-26 18:06:54 -0500

    [1.0][sfSympalPlugin] See previous commit - cleaning up plugin enabler.

--- lib/sfSympalPluginEnabler.class.php
+++ lib/sfSympalPluginEnabler.class.php
@@ -3,8 +3,9 @@
 /**
  * Class responsible for enabling, disabling and overriding Sympal plugins
  *
- * @package sfSympalPlugin
- * @author Jonathan H. Wage <jonwage@gmail.com>
+ * @package   sfSympalPlugin
+ * @author    Jonathan H. Wage <jonwage@gmail.com>
+ * @author    Ryan Weaver <ryan.weaver@iostudio.com>
  */
 class sfSympalPluginEnabler
 {
@@ -13,7 +14,12 @@ class sfSympalPluginEnabler
     $_isSympalEnabled = null,
     $_sympalPluginPath = null;
 
-  public function __construct(ProjectConfiguration $configuration)
+  /**
+   * Class Constructor
+   * 
+   * @param sfProjectConfiguration $configuration The project configuration onto which to enable the plugin
+   */
+  public function __construct(sfProjectConfiguration $configuration)
   {
     $this->_configuration = $configuration;
     $this->_sympalPluginPath = realpath(dirname(__FILE__).'/..');
@@ -21,6 +27,7 @@ class sfSympalPluginEnabler
 
   /**
    * Check whether or not Sympal is enabled for the current application
+   * (or "project" if there is no application in this scope)
    *
    * @return boolean
    */
@@ -29,8 +36,9 @@ class sfSympalPluginEnabler
     if ($this->_isSympalEnabled === null)
     {
       $this->_isSympalEnabled = true;
-      if ($application = sfConfig::get('sf_app'))
+      if ($this->_configuration instanceof sfApplicationConfiguration)
       {
+        $application = $this->_configuration->getApplication();
         $reflection = new ReflectionClass($application.'Configuration');
         if ($reflection->getConstant('disableSympal'))
         {
@@ -38,23 +46,31 @@ class sfSympalPluginEnabler
         }
       }
     }
+
     return $this->_isSympalEnabled;
   }
 
   /**
    * Enable all Sympal plugins
+   * 
+   * This will enable:
+   *   1) Any plugin inside the lib/plugins directory of sfSympalPlugin
+   *      that is in the sfSympalPluginConfiguration::corePlugins array
+   *   2) Any plugin living inside the project's plugins directory
    *
    * @return boolean Returns false if Sympal is not enabled
    */
   public function enableSympalPlugins()
   {
-    $this->_configuration->enablePlugins('sfDoctrinePlugin');
-
     if (!$this->isSympalEnabled())
     {
       return false;
     }
+    
+    // enable sfDoctrinePlugin
+    $this->_configuration->enablePlugins('sfDoctrinePlugin');
 
+    // enable sfSympalPlugin
     $this->_configuration->enablePlugins('sfSympalPlugin');
     $this->_configuration->setPluginPath('sfSympalPlugin', $this->_sympalPluginPath);
 
@@ -70,20 +86,34 @@ class sfSympalPluginEnabler
     $plugins = array_merge($plugins, $foundPlugins);
     $plugins = array_unique($plugins);
     $this->_configuration->setPlugins($plugins);
+    
+    return true;
   }
 
   /**
    * Enable an array of Sympal core plugins
+   * 
+   * A "core" plugin is any plugin that physically lives inside of the
+   * lib/plugins directory of sfSympalPlugin
    *
-   * @param array $plugins
+   * @param array $plugins The core plugins to enable
    * @return void
    */
   public function enableSympalCorePlugins($plugins)
   {
     foreach ((array) $plugins as $plugin)
     {
+      $path = $this->_sympalPluginPath.'/lib/plugins/'.$plugin;
+      if (!file_exists($path))
+      {
+        throw new sfException(sprintf(
+          'Cannot enable core plugin "%s" - it does not exist in %s',
+          $plugin,
+          dirname($path)
+        ));
+      }
       $this->_configuration->enablePlugins($plugin);
-      $this->_configuration->setPluginPath($plugin, $this->_sympalPluginPath.'/lib/plugins/'.$plugin);
+      $this->_configuration->setPluginPath($plugin, $path);
     }
   }
 
@@ -105,18 +135,20 @@ class sfSympalPluginEnabler
   /**
    * Override a Sympal plugin with a new plugin
    *
-   * @param string $plugin 
-   * @param string $newPlugin 
-   * @param string $newPluginPath 
+   * @param string $plugin          The plugin to override
+   * @param string $newPluginPath   The path of the new plugin (defaults to the plugins directory)
    * @return void
    */
-  public function overrideSympalPlugin($plugin, $newPlugin, $newPluginPath = null)
+  public function overrideSympalPlugin($plugin, $newPluginPath = null)
   {
     $this->_configuration->disablePlugins($plugin);
-    $this->_configuration->enablePlugins($newPlugin);
-    if ($newPluginPath)
+    $this->_configuration->enablePlugins($plugin);
+    
+    if ($newPluginPath === null)
     {
-      $this->_configuration->setPluginPath($newPlugin, $newPluginPath);
+      $newPluginPath = sfConfig::get('sf_plugins_dir').'/'.$plugin;
     }
+
+    $this->_configuration->setPluginPath($plugin, $newPluginPath);
   }
 }
