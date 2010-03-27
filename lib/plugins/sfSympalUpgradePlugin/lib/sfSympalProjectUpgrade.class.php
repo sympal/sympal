0dfbfb11c6b506f247c25c0b356b91c80110fc30
Ryan Weaver <ryan@thatsquality.com>
2010-03-26 21:32:11 -0500

    [1.0][sfSympalUpgradePlugin] Throwing better error messages and adding comments and phpDoc

--- lib/plugins/sfSympalUpgradePlugin/lib/sfSympalProjectUpgrade.class.php
+++ lib/plugins/sfSympalUpgradePlugin/lib/sfSympalProjectUpgrade.class.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * Upgrades your project to be compatible from one version of sympal to another
+ * 
+ * @package     sfSympalUpgradePlugin
+ * @subpackage  upgrade
+ * @author      Ryan Weaver <ryan@thatsquality.com>
+ * @since       2010-03-26
+ * @version     svn:$Id$ $Author$
+ */
 class sfSympalProjectUpgrade extends sfSympalUpgrade
 {
   protected
@@ -15,6 +24,14 @@ class sfSympalProjectUpgrade extends sfSympalUpgrade
     return count($this->getUpgrades());
   }
 
+  /**
+   * Returns an array of all possible upgrade files as an array with
+   * the upgrade's key and version
+   * 
+   * This first looks for all .php files in the versions directory.
+   * The files should have the format of VERSION_NUMBER (e.g. 1.0.0__1).
+   * The project is then checked to see if 
+   */
   public function getUpgrades()
   {
     if (!$this->_upgrades)
@@ -26,6 +43,11 @@ class sfSympalProjectUpgrade extends sfSympalUpgrade
       foreach ($files as $file)
       {
         $info = pathinfo($file);
+        if (strpos($info['filename'], '__') === false)
+        {
+          throw new sfException(sprintf('Invalid upgrade filename format for file "%s" - must contain a double underscore - VERSION__NUMBER (e.g. 1.0.0__1) ', $info['filename']));
+        }
+        
         $e = explode('__', $info['filename']);
         $version = $e[0];
         $number = $e[1];
@@ -72,6 +94,11 @@ class sfSympalProjectUpgrade extends sfSympalUpgrade
       $this->logSection('sympal', sprintf('...executing %s upgrade #%s', $upgrade['version'], $upgrade['number']));
 
       $upgradeClass = 'sfSympalUpgrade'.str_replace('.', '_', $upgrade['version'].'__'.$upgrade['number']);
+      if (!class_exists($upgradeClass))
+      {
+        throw new sfException(sprintf('Cannot find upgrade class for upgrade "%s" "#%s" - expecting "%s"', $upgrade['version'], $upgrade['number'], $upgradeClass));
+      }
+      
       $upgradeInstance = new $upgradeClass($this->_configuration, $this->_dispatcher, $this->_formatter);
       $upgradeInstance->setVersion($upgrade['version']);
       $upgradeInstance->setNumber($upgrade['number']);
