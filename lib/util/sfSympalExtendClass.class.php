f41a653755037703e41abbfd17107e35deb85310
Ryan Weaver <ryan@thatsquality.com>
2010-03-29 00:04:42 -0500

    [1.0][sfSympalPlugin] Making slight changes to sfSympalExtendClass, changing event name.
    Two changes
      1) Removed the setProcessed() method. This is done by the event dispatcher based on the return value of your listener.
      2) Removing the 'sympal.' prefix from the method_not_found event. This is unecessary - class names form their own prefixes. For example, sfSympalConfiguration would be sf_sympal_configuration.method_not_found

--- lib/util/sfSympalExtendClass.class.php
+++ lib/util/sfSympalExtendClass.class.php
@@ -27,7 +27,6 @@ class sfSympalExtendClass implements ArrayAccess
     {
       $result = call_user_func_array(array($this, $method), $arguments);
 
-      $event->setProcessed(true);
       $event->setReturnValue($result);
 
       return true;
@@ -41,7 +40,7 @@ class sfSympalExtendClass implements ArrayAccess
   /**
    * Can be used inside a magic __call method to allow for a class to be extended
    * 
-   * This method will throw a sympal.class_name.method_not_found event,
+   * This method will throw a class_name.method_not_found event,
    * where class_name is the "tablelized" class name.
    * 
    * @example
@@ -57,7 +56,7 @@ class sfSympalExtendClass implements ArrayAccess
   public static function extendEvent($subject, $method, $arguments)
   {
     $name = sfInflector::tableize(get_class($subject));
-    $event = ProjectConfiguration::getActive()->getEventDispatcher()->notifyUntil(new sfEvent($subject, 'sympal.'.$name.'.method_not_found', array('method' => $method, 'arguments' => $arguments)));
+    $event = ProjectConfiguration::getActive()->getEventDispatcher()->notifyUntil(new sfEvent($subject, $name.'.method_not_found', array('method' => $method, 'arguments' => $arguments)));
     if (!$event->isProcessed())
     {
       throw new sfException(sprintf('Call to undefined method %s::%s.', get_class($subject), $method));
