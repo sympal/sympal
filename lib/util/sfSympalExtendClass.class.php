485c7b4bf0d89497c820067b819a42a1acfbf3e8
Ryan Weaver <ryan@thatsquality.com>
2010-03-28 20:32:51 -0500

    [1.0][sfSympalPlugin] Adding phpDoc and a little reorganization to sfSympalExtendClass

--- lib/util/sfSympalExtendClass.class.php
+++ lib/util/sfSympalExtendClass.class.php
@@ -10,12 +10,72 @@ class sfSympalExtendClass implements ArrayAccess
 {
   protected $_subject;
 
+  /**
+   * Listener method for method_not_found events
+   * 
+   * @example
+   * $extendedUser = new myExtendedUser(); // extends sfExtendClass
+   * $dispatcher->connect('user.method_not_found', array($extendedUser, 'extend'));
+   */
+  public function extend(sfEvent $event)
+  {
+    $this->_subject = $event->getSubject();
+    $method = $event['method'];
+    $arguments = $event['arguments'];
+
+    if (method_exists($this, $method))
+    {
+      $result = call_user_func_array(array($this, $method), $arguments);
+
+      $event->setProcessed(true);
+      $event->setReturnValue($result);
+
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+
+  /**
+   * Can be used inside a magic __call method to allow for a class to be extended
+   * 
+   * This method will throw a sympal.class_name.method_not_found event,
+   * where class_name is the "tablelized" class name.
+   * 
+   * @example
+   * public function __call($method, $arguments)
+   * {
+   *   sfSympalExtendClass::extendEvent($this, $method, $arguments);
+   * }
+   * 
+   * @param mixed $subject Instance of the class being extended
+   * @param string $method The current method being called
+   * @param array $arguments The arguments being passed to the above methid
+   */
+  public static function extendEvent($subject, $method, $arguments)
+  {
+    $name = sfInflector::tableize(get_class($subject));
+    $event = ProjectConfiguration::getActive()->getEventDispatcher()->notifyUntil(new sfEvent($subject, 'sympal.'.$name.'.method_not_found', array('method' => $method, 'arguments' => $arguments)));
+    if (!$event->isProcessed())
+    {
+      throw new sfException(sprintf('Call to undefined method %s::%s.', get_class($subject), $method));
+    }
+
+    return $event->getReturnValue();
+  }
+
   public function offsetExists($name)
   {
-    try {
+    try
+    {
       $this->__get($name);
+
       return true;
-    } catch (sfException $e) {
+    }
+    catch (sfException $e)
+    {
       return false;
     }
   }
@@ -50,37 +110,10 @@ class sfSympalExtendClass implements ArrayAccess
     $this->_subject->$name = $value;
   }
 
-  public static function extendEvent($subject, $method, $arguments)
-  {
-    $name = sfInflector::tableize(get_class($subject));
-    $event = ProjectConfiguration::getActive()->getEventDispatcher()->notifyUntil(new sfEvent($subject, 'sympal.'.$name.'.method_not_found', array('method' => $method, 'arguments' => $arguments)));
-    if (!$event->isProcessed())
-    {
-      throw new Doctrine_Record_UnknownPropertyException(sprintf('Call to undefined method %s::%s.', get_class($subject), $method));
-    }
-
-    return $event->getReturnValue();
-  }
-
-  public function extend(sfEvent $event)
-  {
-    $this->_subject = $event->getSubject();
-    $method = $event['method'];
-    $arguments = $event['arguments'];
-
-    if (method_exists($this, $method))
-    {
-      $result = call_user_func_array(array($this, $method), $arguments);
-
-      $event->setProcessed(true);
-      $event->setReturnValue($result);
-
-      return true;
-    } else {
-      return false;
-    }
-  }
-
+  /**
+   * Allows methods to be called directly onto this object and have them
+   * be passed back to the original subject
+   */
   public function __call($method, $arguments)
   {
     return call_user_func_array(array($this->_subject, $method), $arguments);
