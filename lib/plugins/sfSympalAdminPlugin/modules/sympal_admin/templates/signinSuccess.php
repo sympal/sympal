0980cbac15dd360e049229661c71987a36f07e1b
Ryan Weaver <ryan@thatsquality.com>
2010-04-03 09:04:35 -0500

    [1.0][sfSympalPlugin] Styling and reworking the login box.

--- lib/plugins/sfSympalAdminPlugin/modules/sympal_admin/templates/signinSuccess.php
+++ lib/plugins/sfSympalAdminPlugin/modules/sympal_admin/templates/signinSuccess.php
@@ -4,18 +4,19 @@
   <h1><?php echo __('Signin'); ?></h1>
 
   <form action="<?php echo url_for('@sympal_admin') ?>" method="post">
-    <table>
-      <tbody>
-        <?php echo $form ?>
-      </tbody>
-      <tfoot>
-        <tr>
-          <td colspan="2">
-            <input type="submit" value="<?php echo __('Signin', null, 'sf_guard') ?>" />
-          </td>
-        </tr>
-      </tfoot>
-    </table>
+    <?php echo $form->renderHiddenFields() ?>
+    <?php echo $form->renderGlobalErrors() ?>
+    <div class="form_row<?php echo $form['username']->hasError() ? ' form_row_error' : '' ?>">
+      <?php echo $form['username']->renderLabel() ?><br/>
+      <?php echo $form['username']->renderError() ?>
+      <?php echo $form['username']->render() ?>
+    </div>
+    <div class="form_row<?php echo $form['username']->hasError() ? ' form_row_error' : '' ?>">
+      <?php echo $form['password']->renderLabel() ?><br/>
+      <?php echo $form['password']->renderError() ?>
+      <?php echo $form['password']->render() ?>
+    </div>
+    <input type="submit" value="<?php echo __('Signin', null, 'sf_guard') ?>" />
   </form>
 </div>
 
