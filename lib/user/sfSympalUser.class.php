a1dc36dcd82ac08a2a9be61ca5e9f7b45d2741c7
Ryan Weaver <ryan@thatsquality.com>
2010-04-02 23:04:59 -0500

    [1.0][sfSympalPlugin] Removing logic from sfSympalUser - it's being migrated to sfSympalThemePlugin.

--- lib/user/sfSympalUser.class.php
+++ lib/user/sfSympalUser.class.php
@@ -130,27 +130,6 @@ class sfSympalUser extends sfGuardSecurityUser
   }
 
   /**
-   * Set the current theme for the users session
-   *
-   * @param string $theme
-   * @return void
-   */
-  public function setCurrentTheme($theme)
-  {
-    $this->setAttribute('sympal_current_theme', $theme);
-  }
-
-  /**
-   * Get the current theme for the users session
-   *
-   * @return string $theme
-   */
-  public function getCurrentTheme()
-  {
-    return $this->getAttribute('sympal_current_theme');
-  }
-
-  /**
    * Get the sfGuardUser instance for the logged in user
    *
    * @return sfGuardUser $guardUser
