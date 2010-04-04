623cd08b774a89ff28ad226802d7421fc5e2ce4f
Ryan Weaver <ryan@thatsquality.com>
2010-04-03 23:24:42 -0500

    [1.0][sfSympalPlugin][test] Moving the toolkit test

--- test/unit/util/sfSympalToolkitTest.php
+++ test/unit/util/sfSympalToolkitTest.php
@@ -1,9 +1,9 @@
 <?php
 
 $app = 'sympal';
-require_once(dirname(__FILE__).'/../bootstrap/unit.php');
+require_once(dirname(__FILE__).'/../../bootstrap/unit.php');
 
-$t = new lime_test(5);
+$t = new lime_test(4);
 
 $sympalContext = sfSympalContext::getInstance();
 $t->is($sympalContext->getService('site_manager')->getSite()->getSlug(), $app, '->The current site matches the current app');
