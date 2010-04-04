d9af0c8bb7c3f75ec8d8f92f30cd9e54ca947e6e
Ryan Weaver <ryan@thatsquality.com>
2010-04-04 00:45:04 -0500

    [1.0][sfSympalContentListPlugin] Fixing up test - the Site and title are now required, not sure why they weren't before.

--- test/unit/sfSympalContentListPlugin/model/doctrine/PluginsfSympalContentListTest.php
+++ test/unit/sfSympalContentListPlugin/model/doctrine/PluginsfSympalContentListTest.php
@@ -3,16 +3,22 @@
 $app = 'sympal';
 require_once(dirname(__FILE__).'/../../../../bootstrap/unit.php');
 
-$t = new lime_test(2);
+$t = new lime_test(0);
+
+$site = Doctrine_Core::getTable('sfSympalSite')->findOneBySlug($app);
 
 $page = sfSympalContent::createNew('sfSympalPage');
 $page->date_published = date('Y-m-d h:i:s', time() + 86400); // tomorrow
+$page->Site = $site;
+$page->title = 'My test page';
 $page->save();
 
 $pageContentType = Doctrine_Core::getTable('sfSympalContentType')->findOneByName('sfSympalPage');
 
 $contentList = sfSympalContent::createNew('sfSympalContentList');
 $contentList->ContentType = $pageContentType;
+$contentList->Site = $site;
+$contentList->title = 'My test content list';
 $contentList->save();
 
 /**
