50babffcc4ac8012b7e10b583efb1a994ae0b746
Ryan Weaver <ryan@thatsquality.com>
2010-04-22 19:13:23 -0500

    [sfSympalAdminPlugin] phpDoc

--- lib/plugins/sfSympalAdminPlugin/modules/sympal_admin/lib/Basesympal_adminActions.class.php
+++ lib/plugins/sfSympalAdminPlugin/modules/sympal_admin/lib/Basesympal_adminActions.class.php
@@ -1,12 +1,11 @@
 <?php
 
 /**
- * Base actions for the sfSympalAdminPlugin sympal_admin module.
+ * Provides basic admin actions like clear cache, php info, etc
  * 
  * @package     sfSympalAdminPlugin
- * @subpackage  sympal_admin
- * @author      Your name here
- * @version     SVN: $Id: BaseActions.class.php 12534 2008-11-01 13:38:27Z Kris.Wallsmith $
+ * @subpackage  actions
+ * @author      Jonathan H. Wage <jonwage@gmail.com>
  */
 abstract class Basesympal_adminActions extends sfActions
 {
