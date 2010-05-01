2ac02e6ac59d156ddf554f0f42bdc727a030531e
Ryan Weaver <ryan@thatsquality.com>
2010-04-30 22:12:28 -0500

    Adding the two new plugins to the core plugin list and updating those submodules.

--- config/sfSympalPluginConfiguration.class.php
+++ config/sfSympalPluginConfiguration.class.php
@@ -345,5 +345,7 @@ class sfSympalPluginConfiguration extends sfPluginConfiguration
       'sfSympalThemePlugin',
       'sfSympalMinifyPlugin',
       'sfSympalFormPlugin',
+      'sfInlineObjectPlugin',
+      'sfContentFilterPlugin',
     );
 }
