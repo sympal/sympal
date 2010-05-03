abd7ca11373501f87ae0059a93ff966bf555b983
Ryan Weaver <ryan@thatsquality.com>
2010-05-02 23:17:24 -0500

    Enabling xml output for the prove file.

--- test/bin/prove.php
+++ test/bin/prove.php
@@ -5,4 +5,12 @@ include dirname(__FILE__).'/../bootstrap/unit.php';
 $h = new lime_harness();
 $h->register(sfFinder::type('file')->name('*Test.php')->in(dirname(__FILE__).'/..'));
 
-exit($h->run() ? 0 : 1);
+$ret = $h->run() ? 0 : 1;
+
+if (isset($argv[1]) && strpos($argv[1],'--xml=') === 0)
+{
+  $file = str_replace('--xml=', '', $argv[1]);
+  file_put_contents($file, $h->to_xml());
+}
+
+exit($ret);
