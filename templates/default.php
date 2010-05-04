d15b911ca1026bd158965e313484c2817e028971
Ryan Weaver <ryan@thatsquality.com>
2010-05-03 20:27:46 -0500

    [theme] Fixing the default theme - removing conditional code placed earlier.

--- templates/default.php
+++ templates/default.php
@@ -1,4 +1,3 @@
-<?php $cmfInstalled = $sf_sympal_context->getSympalConfiguration()->pluginExists('sfSympalCMFPlugin') ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
  "http://www.w3.org/TR/html4/strict.dtd">
 <html>
@@ -13,18 +12,16 @@
 <body>
   <div id="container">
   	<div id="header">
-  	  <h1><?php echo link_to($cmfInstalled ? $sf_sympal_site->getTitle() : 'Homepage', '@homepage') ?></h1>
-  	  <h2><?php echo $cmfInstalled ? $sf_sympal_site->getDescription() : 'Site Description...' ?></h2>
+  	  <h1><?php echo link_to($sf_sympal_site->getTitle(), '@homepage') ?></h1>
+  	  <h2><?php echo $sf_sympal_site->getDescription() ?></h2>
   	</div>
   	<div id="content">
       <?php echo $sf_content ?>
   	</div>
-  	<?php $menu = $cmfInstalled ? get_sympal_menu('primary') : false; ?>
+  	<?php $menu = get_sympal_menu('primary') ?>
   	<div id="sidebar">
-      <?php if ($cmfInstalled): ?>
-        <h2><?php echo __('Search') ?></h2>
-        <?php echo get_partial('sympal_search/form') ?>
-      <?php endif; ?>
+      <h2><?php echo __('Search') ?></h2>
+      <?php echo get_partial('sympal_search/form') ?>
 
       <?php if (has_slot('sympal_right_sidebar')): ?>
         <?php echo get_slot('sympal_right_sidebar') ?>
@@ -36,16 +33,11 @@
         <?php echo $menu->render() ?>
       <?php else: ?>
         <ul>
-          <?php if ($cmfInstalled): ?>
-            <?php if (!$sf_user->isAuthenticated()): ?>
-              <li><?php echo link_to(__('Register'), '@sympal_register') ?></li>
-              <li><?php echo link_to(__('Signin'), '@sympal_signin') ?></li>
-            <?php else: ?>
-              <li><?php echo link_to(__('Signout'), '@sympal_signout', 'confirm='.__('Are you sure?')) ?></li>
-            <?php endif; ?>
+          <?php if (!$sf_user->isAuthenticated()): ?>
+            <li><?php echo link_to(__('Register'), '@sympal_register') ?></li>
+            <li><?php echo link_to(__('Signin'), '@sympal_signin') ?></li>
           <?php else: ?>
-            <li><a href="http://symfony-project.org">symfony</a></li>
-            <li><a href="http://sympalphp.org">Sympal</a></li>
+            <li><?php echo link_to(__('Signout'), '@sympal_signout', 'confirm='.__('Are you sure?')) ?></li>
           <?php endif; ?>
         </ul>
       <?php endif; ?>
