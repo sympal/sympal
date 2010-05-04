a1cc90b0d320c1919f84eca17dd09547178aace9
Ryan Weaver <ryan@thatsquality.com>
2010-05-03 19:25:11 -0500

    [test] Fixing theme app.yml configuration in test dir for sfThemePlugin and adding some comments to the Actions test.

--- test/functional/ActionsTest.php
+++ test/functional/ActionsTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * A functional test for sfSympalActions - as close to a unit test as
+ * we could get
+ * 
+ * @package     sfSympalPlugin 
+ * @subpackage  test
+ * @author      Jonathan H. Wage <jonwage@gmail.com>
+ * @author      Ryan Weaver <ryan@thatsquality.com>
+ */
 require_once(dirname(__FILE__).'/../bootstrap/functional.php');
 
 $browser = new sfSympalTestFunctional(new sfBrowser());
@@ -33,7 +42,9 @@ $browser->info('1 - Test the ask confirmation action')
   ->end()
 ;
 
-$browser
+$browser->info('2 - Test the ->forwardToRoute() method')
+
+  ->info('  2.1 - Test using the ?var=val query params')
   ->get('/test/forward_to_route')
 
   ->with('request')->begin()
@@ -42,9 +53,8 @@ $browser
     ->isParameter('param1', 'value1')
     ->isParameter('param2', 'value2')
   ->end()
-;
 
-$browser
+  ->info('  2.2 - Test using the array formation of variables')
   ->get('/test/forward_to_route2')
 
   ->with('request')->begin()
@@ -55,30 +65,35 @@ $browser
   ->end()
 ;
 
-$browser
+$browser->info('3 - Test the ->goBack() method')
   ->get('/test/start_go_back')
   ->get('/test/go_back')
 
   ->with('response')->begin()
+    ->info('  3.1 - Using ->goBack() causes a redirect')
     ->isRedirected()
     ->followRedirect()
-
   ->end()
+
   ->with('request')->begin()
+    ->info('  3.2 - The final resting place is the original action')
     ->isParameter('module', 'test')
     ->isParameter('action', 'start_go_back')
   ->end()
 ;
 
-$browser->get('/test/change_layout');
+
+$browser->info('4 - Set the layout in an action')
+  ->get('/test/change_layout')
+;
 
 $response = $browser->getResponse();
-
 $stylesheets = $response->getStylesheets();
-$browser->test()->is(isset($stylesheets['test']), true);
+$browser->test()->is(isset($stylesheets['test']), true, 'The response contains the "test" stylesheet');
 
 $browser
   ->with('response')->begin()
+    ->info('  4.1 - The correct layout is rendered')
     ->checkElement('h1', '/Test Layout/')
   ->end()
 ;
