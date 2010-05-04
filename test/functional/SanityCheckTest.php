bcf3198671a91aed7b26a5385aebb55c0939bb75
Ryan Weaver <ryan@thatsquality.com>
2010-05-03 20:28:11 -0500

    [test] Adding comments to the SanityCheck test.

--- test/functional/SanityCheckTest.php
+++ test/functional/SanityCheckTest.php
@@ -6,8 +6,9 @@ $browser = new sfSympalTestFunctional(new sfBrowser());
 $browser->signInAsAdmin();
 $browser->get('/');
 
-// Make sure we can click each menu item in the menu
+$browser->info('1 - Test that all the menu items display with a link');
 $menuItems = Doctrine_Core::getTable('sfSympalMenuItem')->findAll();
+$i = 1;
 foreach ($menuItems as $menuItem)
 {
   if ($menuItem->level <= 0 || $menuItem->requires_auth || $menuItem->requires_no_auth || !($content = $menuItem->getContent()))
@@ -16,18 +17,20 @@ foreach ($menuItems as $menuItem)
   }
 
   $browser
+    ->info(sprintf('  1.%s - Checking %s menu item', $i++, $menuItem->getLabel()))
+
     ->click($menuItem->getLabel())
     ->with('response')->begin()
-    ->  isStatusCode('200')
+      ->isStatusCode('200')
     ->end()
     ->with('request')->begin()
-    ->  isParameter('module', 'sympal_content_renderer')
-    ->  isParameter('action', 'index')
+      ->isParameter('module', 'sympal_content_renderer')
+      ->isParameter('action', 'index')
     ->end()
   ;
 }
 
-$browser
+$browser->info('2 - Testing the login to the admin')
   ->get('/security/signin')
   ->click('input[type="submit"]', array('signin' => array('username' => 'admin', 'password' => 'admin')), array('method' => 'post', '_with_csrf' => true))
   ->with('response')->begin()
@@ -43,19 +46,18 @@ $browser
     ->isParameter('action', 'index')
   ->end()
 ;
-
 $browser->signOut();
 
-$browser
+
+$browser->info('3 - Testing the registration form')
   ->get('/register')
   ->click('input[type="submit"]', array('sf_guard_user' => array('first_name' => 'Jonathan', 'last_name' => 'Wage', 'email_address' => 'jonathan.wage@sensio.com', 'username' => 'test', 'password' => 'test', 'password_again' => 'test')), array('method' => 'post', '_with_csrf' => true))
   ->with('response')->begin()
     ->isRedirected()
     ->followRedirect()
   ->end()
-;
 
-$browser
+  ->info('  3.1 - Attempt to signin after the registration')
   ->signOut()
   ->click('Signin')
   ->click('input[type="submit"]', array('signin' => array('username' => 'test', 'password' => 'test')), array('method' => 'post', '_with_csrf' => true))
@@ -65,6 +67,8 @@ $browser
   ->signOut()
 ;
 
+
+$browser->info('4 - Test that loading a page takes only one query');
 $profiler = new Doctrine_Connection_Profiler();
 $conn = Doctrine_Manager::connection();
 $conn->addListener($profiler);
