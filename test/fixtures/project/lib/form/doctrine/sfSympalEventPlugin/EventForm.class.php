b5864c866c1f03c2d455722adbb5603b14afd8eb
Maxim Tsepkov <max@cluster-studio.com>
2010-05-07 00:31:24 +0600

    sympal:install the test project

---
+++ test/fixtures/project/lib/form/doctrine/sfSympalEventPlugin/EventForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Event form.
+ *
+ * @package    sympal
+ * @subpackage form
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrinePluginFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class EventForm extends PluginEventForm
+{
+  public function configure()
+  {
+  }
+}
