e0a087e72b29305a65214d5048e5dfd20d785d9a
Ryan Weaver <ryan@thatsquality.com>
2010-05-08 18:15:54 -0500

    [test] Rearranging functional bootstrap so that things bootstrap before potentially refreshing the assets via cleanup.php

--- test/bootstrap/functional.php
+++ test/bootstrap/functional.php
@@ -5,13 +5,12 @@ if (!isset($app))
   $app = 'sympal';
 }
 
-require_once(dirname(__FILE__).'/cleanup.php');
-
 require_once dirname(__FILE__).'/../fixtures/project/config/ProjectConfiguration.class.php';
 $configuration = ProjectConfiguration::getApplicationConfiguration($app, 'test', isset($debug) ? $debug : true);
 
 require_once(sfConfig::get('sf_lib_dir').'/sfSympalTestFunctional.class.php');
 
 sfContext::createInstance($configuration);
-
 $configuration->initializeSympal();
+
+require_once(dirname(__FILE__).'/cleanup.php');
