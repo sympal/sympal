269f900c3cca2a439f18128fd972caee54f17509
Ryan Weaver <ryan@thatsquality.com>
2010-05-16 11:48:17 -0500

    Adding phpDoc

--- lib/plugins/sfSympalRenderingPlugin/modules/sympal_content_renderer/lib/Basesympal_content_rendererActions.class.php
+++ lib/plugins/sfSympalRenderingPlugin/modules/sympal_content_renderer/lib/Basesympal_content_rendererActions.class.php
@@ -1,7 +1,22 @@
 <?php
 
+/**
+ * Actions class for the rendering of most Content records
+ * 
+ * @package     sfSympalRenderingPlugin
+ * @subpackage  actions
+ * @author      Jonathan H. Wage <jonwage@gmail.com>
+ * @author      Ryan Weaver <ryan@thatsquality.com>
+ */
 class Basesympal_content_rendererActions extends sfActions
 {
+
+  /**
+   * Specified in app.yml as default_rendering_module and default_rendering_action
+   * 
+   * By default, all Content routes are pointed here. The content action
+   * loader then processes everything needed to render the content
+   */
   public function executeIndex(sfWebRequest $request)
   {
     $this->renderer = $this->getSympalContentActionLoader()->loadContentRenderer();
